0ef0a80587c1289dd343a3fd3381b30c064c80f2
JezewskiG <gjwsky@icloud.com>
2022-02-15 05:29:38 +0100

    [Alpha] Working prototype. Lack of user controlls and animated mode.

--- Sources/App/Views/Interface/UserInterfaceView.swift
+++ Sources/App/Views/Interface/UserInterfaceView.swift
@@ -6,14 +6,28 @@
 //
 
 import SwiftUI
+import ModularMTLCore
 
 struct UserInterfaceView: View {
+    
+    @EnvironmentObject var data: UIDataObject
+    
     var body: some View {
-        VStack {
+        VStack(alignment: .center, spacing: 20) {
             Text("MODULAR")
                 .font(.system(size: 40))
                 .fontWeight(.ultraLight)
                 .tracking(40)
+            
+            Text("N: \(data.pointsCount)")
+                .font(.system(size: 30))
+                .fontWeight(.ultraLight)
+                .tracking(5)
+            
+            Text("M: \(String(format: "%.2f", data.multiplier))")
+                .font(.system(size: 30))
+                .fontWeight(.ultraLight)
+                .tracking(5)
         }
     }
 }
--- Sources/Core/Sources/Renderer/VisualiserRenderer.swift
+++ Sources/Core/Sources/Renderer/VisualiserRenderer.swift
@@ -15,13 +15,22 @@ public class VisualiserRenderer: Renderer {
     
     var data: UIDataObject
     var library: Library
-    var pointsBuffer: PointsBuffer<simd_float2>
+    
+    var pointsBuffer: ManagedBuffer<simd_float2>
+    var linesBuffer: ManagedBuffer<simd_float4>
+    
+    let fillBufferPSO: MTLComputePipelineState
+    let fillStaticPSO: MTLComputePipelineState
+    let fillAnimatedPSO: MTLComputePipelineState
+    
+    let renderPassPSO: MTLRenderPipelineState
     
     public init(with device: MTLDevice, _ data: UIDataObject) {
         self.device = device
         self.data = data
         self.library = Library(with: device,
-                               functions: ["animated"])
+                               functions: ["fillPointsBuffer", "fillAnimatedLines", "fillStaticLines",
+                                           "fragmentFunction", "pointsVertexFunction", "linesVertexFunction"])
 
         guard let texture = TextureManager.getTexture(with: device,
                                                       format: .bgra8Unorm_srgb,
@@ -32,11 +41,102 @@ public class VisualiserRenderer: Renderer {
         }
         
         self.renderTargetTexture = texture
-        self.pointsBuffer = PointsBuffer(with: device, size: data.pointsCount)
+        
+        let minimum = UInt(500)
+        self.pointsBuffer = ManagedBuffer(with: device, count: data.pointsCount, minimum: minimum)
+        self.linesBuffer = ManagedBuffer(with: device, count: data.pointsCount, minimum: minimum)
+        
+        do {
+            self.fillBufferPSO = try device.makeComputePipelineState(function: library.getFunction(name: "fillPointsBuffer")!)
+            self.fillStaticPSO = try device.makeComputePipelineState(function: library.getFunction(name: "fillStaticLines")!)
+            self.fillAnimatedPSO = try device.makeComputePipelineState(function: library.getFunction(name: "fillAnimatedLines")!)
+            
+            let renderPipelineDescriptor = MTLRenderPipelineDescriptor()
+            renderPipelineDescriptor.vertexFunction = library.getFunction(name: "linesVertexFunction")
+            renderPipelineDescriptor.fragmentFunction = library.getFunction(name: "fragmentFunction")
+            renderPipelineDescriptor.colorAttachments[0].pixelFormat = MTLPixelFormat.bgra8Unorm_srgb
+            renderPipelineDescriptor.sampleCount = data.sampleCount
+            renderPassPSO = try device.makeRenderPipelineState(descriptor: renderPipelineDescriptor)
+        } catch {
+            print(error)
+            fatalError("[Metal] Error creating PSO.")
+        }
     }
     
-    public func draw(with device: MTLDevice, _ commandBuffer: MTLCommandBuffer) {
-        pointsBuffer.updateStatus(points: data.pointsCount)
+    private func encodeFillBuffer(target buffer: MTLBuffer,
+                                  commandBuffer: MTLCommandBuffer,
+                                  elementCount: simd_uint1) {
+        var pointsCount = elementCount
+        let fillBufferEncoder = commandBuffer.makeComputeCommandEncoder()
+        fillBufferEncoder?.setComputePipelineState(self.fillBufferPSO)
+        fillBufferEncoder?.label = "fillBuffer Compute CE"
+        fillBufferEncoder?.setBytes(&pointsCount, length: MemoryLayout<simd_uint1>.stride, index: 0)
+        fillBufferEncoder?.setBytes(&data.circleRadius, length: MemoryLayout<simd_float1>.stride, index: 1)
+        fillBufferEncoder?.setBuffer(buffer, offset: 0, index: 2)
+        var threadGroupSize = self.fillBufferPSO.maxTotalThreadsPerThreadgroup
+        if threadGroupSize > pointsCount {
+            threadGroupSize = Int(pointsCount)
+        }
+        fillBufferEncoder?.dispatchThreads(MTLSize(width: Int(pointsCount), height: 1, depth: 1),
+                                           threadsPerThreadgroup: MTLSize(width: threadGroupSize, height: 1, depth: 1))
+        fillBufferEncoder?.endEncoding()
+    }
+    
+    private func encodeFillLinesBuffer(from pointsBuffer: MTLBuffer,
+                                       to linesBuffer: MTLBuffer,
+                                       commandBuffer: MTLCommandBuffer,
+                                       elementCount: simd_uint1,
+                                       animated: Bool) {
+       
+        var pointsCount = elementCount
+        let fillBufferEncoder = commandBuffer.makeComputeCommandEncoder()
+        fillBufferEncoder?.setBytes(&pointsCount, length: MemoryLayout<simd_uint1>.stride, index: 0)
+        fillBufferEncoder?.setBuffer(pointsBuffer, offset: 0, index: 2)
+        fillBufferEncoder?.setBuffer(linesBuffer, offset: 0, index: 3)
+        fillBufferEncoder?.label = animated ? "fillAnimated Compute CE" : "fillStatic Compute CE"
+       
+        if animated {
+            fillBufferEncoder?.setComputePipelineState(self.fillAnimatedPSO)
+            fillBufferEncoder?.setBytes(&data.multiplier, length: MemoryLayout<simd_float1>.stride, index: 1)
+        }
+        else {
+            fillBufferEncoder?.setComputePipelineState(self.fillStaticPSO)
+            var integerM = simd_uint1(data.multiplier.rounded())
+            fillBufferEncoder?.setBytes(&integerM, length: MemoryLayout<simd_uint1>.stride, index: 1)
+        }
+        
+        var threadGroupSize = self.fillBufferPSO.maxTotalThreadsPerThreadgroup
+        if threadGroupSize > pointsCount {
+            threadGroupSize = Int(pointsCount)
+        }
+        fillBufferEncoder?.dispatchThreads(MTLSize(width: Int(pointsCount), height: 1, depth: 1),
+                                           threadsPerThreadgroup: MTLSize(width: threadGroupSize, height: 1, depth: 1))
+        fillBufferEncoder?.endEncoding()
+    }
+    
+    public func draw(with device: MTLDevice, _ commandBuffer: MTLCommandBuffer, _ viewRPD: MTLRenderPassDescriptor) {
+        let pointsCount = simd_uint1(data.pointsCount)
+        pointsBuffer.updateStatus(points: UInt(pointsCount))
+        
+        if pointsBuffer.isValid == false {
+            encodeFillBuffer(target: self.pointsBuffer.contents(),
+                             commandBuffer: commandBuffer,
+                             elementCount: pointsCount)
+            
+            encodeFillLinesBuffer(from: pointsBuffer.contents(),
+                                  to: linesBuffer.contents(),
+                                  commandBuffer: commandBuffer,
+                                  elementCount: pointsCount,
+                                  animated: false)
+        }
+        
+        let renderEncoder = commandBuffer.makeRenderCommandEncoder(descriptor: viewRPD)!
+        
+        renderEncoder.setRenderPipelineState(renderPassPSO)
+        renderEncoder.setVertexBuffer(linesBuffer.contents(), offset: 0, index: 0)
+        renderEncoder.drawPrimitives(type: .line, vertexStart: 0, vertexCount: Int(pointsCount*2))
+        renderEncoder.endEncoding()
+        
         return
     }
 }
